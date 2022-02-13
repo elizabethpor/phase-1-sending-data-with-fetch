8920fda5f2cd85e83d0fcccd7ef653cd6dceb033
Elizabeth <elizabeth.por@gmail.com>
2022-02-12 18:13:50 -0600

    commit tree 6c173107e13d7104ca5767a673793a0d375a0d67

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify({name: name, email: email})
+    })
+    .then(response => response.json())
+    .then(object => {
+        console.log(object)
+        console.log(object.id)
+        const body = document.querySelector('body');
+        const p = document.createElement('p');
+        p.textContent = object.id;
+        body.append(p);
+    })
+    .catch(error => {
+        console.log(error.message)
+        const body = document.querySelector('body');
+        const p = document.createElement('p');
+        p.textContent = error.message;
+        body.append(p)
+    });
+
+
+};
+
+submitData('Steve', 'steve@steve.com');
